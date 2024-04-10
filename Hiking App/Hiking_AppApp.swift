c85e8a949d8b67fbc676fc464f9d1898f6d82d9e
Timotius Leonardo <lionato14@gmail.com>
2024-04-10 11:02:20 +0700

    Initial Commit

---
+++ Hiking App/Hiking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hiking_AppApp.swift
+//  Hiking App
+//
+//  Created by Timotius Leonardo Lianoto on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Hiking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
