b5600462a37d70afd1689d219fdc3de62b4424a3
Timotius Leonardo <lionato14@gmail.com>
2024-04-10 12:31:07 +0700

    Initial Commit

--- Hiking App/Component/CardView.swift
+++ Hiking App/Component/CardView.swift
@@ -10,17 +10,51 @@ import SwiftUI
 struct CardView: View {
     var body: some View {
         ZStack {
-            Circle()
-                .fill(
-                    LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")],
-                                   startPoint: .topLeading,
-                                   endPoint: .bottomTrailing)
-                )
-                .frame(width: 256, height: 256)
-            Image("image-1")
-                .resizable()
-            .scaledToFit()
-        }
+            CustomBackgroundView()
+            
+            VStack {
+                // MARK: - HEADER
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                        .foregroundStyle(LinearGradient(colors: [Color.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom))
+                        
+                        Spacer()
+                        
+                        Button {
+                            // ACTION: Show a sheet
+                            print("The button was pressed.")
+                        } label: {
+                            CustomButtonView()
+                        }
+                    }
+                    
+                    Text("Fun and enjoyable outdoor activity for friends and families")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundStyle(Color.customGrayMedium)
+                } //: HEADER
+                .padding(.horizontal, 30)
+                
+                // MARK: - MAIN CONTENT
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")],
+                                           startPoint: .topLeading,
+                                           endPoint: .bottomTrailing)
+                        )
+                        .frame(width: 256, height: 256)
+                    Image("image-1")
+                        .resizable()
+                    .scaledToFit()
+                }
+            }
+        } //: CARD
+        .frame(width: 320, height: 570)
     }
 }
 
--- Hiking App/Component/CustomBackgroundView.swift
+++ Hiking App/Component/CustomBackgroundView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct CustomBackgroundView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // MARK: - 3. DEPTH
+            Color.customGreenDark
+                .clipShape(RoundedRectangle(cornerRadius: 40))
+                .offset(y: 12)
+            
+            // MARK: - 2. LIGHT
+            Color.customGreenLight
+                .clipShape(RoundedRectangle(cornerRadius: 40))
+                .offset(y: 3)
+                .opacity(0.85)
+            
+            // MARK: - 1. SURFACE
+            Color.customGreenDark
+                .clipShape(RoundedRectangle(cornerRadius: 40))
+                .offset(y: 12)
+            
+            LinearGradient(
+                colors: [
+                    Color.customGreenLight,
+                    Color.customGreenMedium
+                ],
+                startPoint: .top,
+                endPoint: .bottom)
+            .clipShape(RoundedRectangle(cornerRadius: 40))
+        }
     }
 }
 
