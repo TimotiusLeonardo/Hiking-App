a4943ba4acd7af21e49522508f5329cedc5edbe9
Timotius Leonardo <lionato14@gmail.com>
2024-04-11 15:45:47 +0700

    add background animation

---
+++ Hiking App/Component/CustomCircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCircleView.swift
+//  Hiking App
+//
+//  Created by Timotius Leonardo Lianoto on 11/04/24.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomCircleView()
+}
